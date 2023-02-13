18fedc4b9e381cf9c3664b2f4c1bd3ec80dde8e7
raisul arefin nahid <analognahid@gmail.com>
2023-02-13 17:51:13 -0600

    update

--- binaries/c_many/main.c
+++ binaries/c_many/main.c
@@ -5,17 +5,17 @@
 
 
 int main() {
-stack *stk = NULL;
-push(7, &stk);
-int temp = add(1,2);
-push(temp, &stk);
+    stack *stk = NULL;
+    push(7, &stk);
+    int temp = add(1,2);
+    push(temp, &stk);
 
-push(9, &stk);
-push(12,&stk);
-printf("%d\n",pop(&stk));
-printf("%d\n",pop(&stk));
-printf("%d\n",pop(&stk));
-printf("%d\n",pop(&stk));
-printf("%d\n",pop(&stk));
-return 0;
+    push(9, &stk);
+    push(12,&stk);
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    printf("%d\n",pop(&stk));
+    return 0;
 }
