db0e2b1d7c91b649638f217616f6948dbb3a7b73
raisul arefin nahid <analognahid@gmail.com>
2023-02-22 12:55:04 -0600

    latest

--- binaries/c_many/calculate.c
+++ binaries/c_many/calculate.c
@@ -3,7 +3,17 @@
 #include <stdlib.h>
 
 int add(int a, int b){
-    return a+b;
+    int ran = 10;
+    ran = ran +12;
+    ran =ran*ran;
+    float fran =0.12;
+    fran = fran+1.0;
+    for (int i = 0; i<10;i++){
+        int fran = 0;
+        fran=fran+2;
+    }
+    ran =ran -10;
+    return a+b+ran;
 }
 float addf(float a, float b){
     return a+b;
