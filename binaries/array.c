5c9d682a9f389f29f3da8552298973a01f00a2ee
raisul arefin nahid <analognahid@gmail.com>
2023-03-13 14:12:45 -0500

    updates

--- binaries/array.c
+++ binaries/array.c
@@ -10,15 +10,20 @@ int main() {
   printf("Enter 5 integers: ");
 
   // taking input and storing it in an array
-  for(int i = 0; i < 5; ++i) {
+  for(int i = 0; i < 5; ++i) {// vanilla loop
+     values[i] = i;
      scanf("%d", &values[i]);
   }
 
   printf("Displaying integers: ");
 
   // printing elements of an array
-  for(int i = 0; i < 5; ++i) {
-     printf("%d\n", values[i]);
+  for(int i = 0; i < 5; ++i) {//loop1
+     values[i] = i;
+     for(int j = 0; j < 5; ++j) {//loop2
+         values[i*j%5] =  i*j%5;
+
+     }
   }
   return 0;
 }
