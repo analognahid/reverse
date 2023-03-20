bff20ade068e58a947211756af6934f68492a829
raisul arefin nahid <analognahid@gmail.com>
2023-03-20 17:02:56 -0500

    presentation

---
+++ codes/dwarf/dummy_dwarf.dart
@@ -0,0 +1,29 @@
+SUBPROGRAM:  dummy
+
+
+
+a_int  (DW_OP_fbreg: -148)       ->36
+
+b_float (DW_OP_fbreg: -152)      ->40
+
+c_double (DW_OP_fbreg: -160)     ->48
+
+char_pointer (DW_OP_fbreg: -168) ->56
+
+e_int  (DW_OP_fbreg: -132)       ->20
+
+f_float  (DW_OP_fbreg: -128)     ->16
+
+g_double (DW_OP_fbreg: -120)     ->8
+x1
+x2
+x3
+x4
+x5
+
+x6 (DW_OP_fbreg: -124)           ->12
+
+int_arr (DW_OP_fbreg: -112)      ->0
+
+k (DW_OP_fbreg: -136)            ->24
+
