96d373c43cda02a7517ac37760365de158177c39
raisul arefin nahid <analognahid@gmail.com>
2023-03-28 16:48:21 -0500

    updates

--- codes/dwarf/pop.dart
+++ codes/dwarf/pop.dart
@@ -1,31 +1,13 @@
-SUBPROGRAM:  dummy
+SUBPROGRAM:  add
 None (DW_OP_fbreg: -4) 0
-a_int
+a
 None (DW_OP_fbreg: -8) 4
-b_float
+b
+None (DW_OP_fbreg: -12) 8
+ran
 None (DW_OP_fbreg: -16) 12
-c_double
+fran
+None (DW_OP_fbreg: -20) 16
+i
 None (DW_OP_fbreg: -24) 20
-char_pointer
-None (DW_OP_fbreg: -28) 24
-e_int
-None (DW_OP_fbreg: -32) 28
-f_float
-None (DW_OP_fbreg: -40) 36
-g_double
-None (DW_OP_fbreg: -44) 40
-x1
-None (DW_OP_fbreg: -48) 44
-x2
-None (DW_OP_fbreg: -52) 48
-x3
-None (DW_OP_fbreg: -56) 52
-x4
-None (DW_OP_fbreg: -60) 56
-x5
-None (DW_OP_fbreg: -64) 60
-x6
-None (DW_OP_fbreg: -144) 140
-int_arr
-None (DW_OP_fbreg: -148) 144
-k
+fran
