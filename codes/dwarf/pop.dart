ce94ea74db9e2abe18075a706c75d3cd8f199681
raisul arefin nahid <analognahid@gmail.com>
2023-03-28 14:21:20 -0500

    die order problem fixed

--- codes/dwarf/pop.dart
+++ codes/dwarf/pop.dart
@@ -1,15 +1,31 @@
-SUBPROGRAM:  pop
-
-
-
-stk_ptr (DW_OP_fbreg: -56)   ->32
-
-int1 (DW_OP_fbreg: -44)      ->20
-
-int2 (DW_OP_fbreg: -40)      ->16
-
-number (DW_OP_fbreg: -36)    ->12
-
-stk (DW_OP_fbreg: -32)       ->8
- 
-tmp (DW_OP_fbreg: -24)       ->0
+SUBPROGRAM:  dummy
+None (DW_OP_fbreg: -4) 0
+a_int
+None (DW_OP_fbreg: -8) 4
+b_float
+None (DW_OP_fbreg: -16) 12
+c_double
+None (DW_OP_fbreg: -24) 20
+char_pointer
+None (DW_OP_fbreg: -28) 24
+e_int
+None (DW_OP_fbreg: -32) 28
+f_float
+None (DW_OP_fbreg: -40) 36
+g_double
+None (DW_OP_fbreg: -44) 40
+x1
+None (DW_OP_fbreg: -48) 44
+x2
+None (DW_OP_fbreg: -52) 48
+x3
+None (DW_OP_fbreg: -56) 52
+x4
+None (DW_OP_fbreg: -60) 56
+x5
+None (DW_OP_fbreg: -64) 60
+x6
+None (DW_OP_fbreg: -144) 140
+int_arr
+None (DW_OP_fbreg: -148) 144
+k
