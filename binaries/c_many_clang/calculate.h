11e1bc50109c57cf5f311efc73f0be2b211f25dd
nahid <analognahid@gmail.com>
2023-03-28 11:01:40 -0500

    changes

--- binaries/c_many_clang/calculate.h
+++
@@ -1,9 +0,0 @@
-
-int add(int a, int b);
-
-float addf(float a, float b);
-
-
-int substract(int a, int b);
-
-float substractf(float a, float b);
--- binaries/c_many_clang/stack.h
+++
@@ -1,9 +0,0 @@
-typedef struct S_stack {
-    int number;
-    struct S_stack *next;
-} stack;
-
-void push(int number, stack **stk_ptr);
- int dummy(int a_int, float b_float, double c_double,  char * char_pointer);
-int pop(stack **stk_ptr);
-int fake(int a , int b, int x);
