b87b034291070d2c26731bd535dd5b4c79a61bb5
analognahid <analognahid@gmail.com>
2024-02-01 15:37:37 -0600

    before usenix changes

--- disassembly/ghidraScript.sh
+++ disassembly/ghidraScript.sh
@@ -1,4 +1,4 @@
-# /home/nahid/reverse_tools/ghidra/ghidra_10.2_PUBLIC_20221101/ghidra_10.2_PUBLIC/support/analyzeHeadless /home/nahid/reverse/disassembly/ghidra_project/ ghidraBenchmarking_MainProcess -import /home/nahid/reverse/codes/ML/data/binaries/array -overwrite -scriptPath /home/nahid/reverse/disassembly -preScript dwarf_line.py -postScript ghidra_only_instructions.py
+# /home/raisul/ghidra_10.3.3_PUBLIC_20230829/ghidra_10.3.3_PUBLIC/support/analyzeHeadless /home/nahid/reverse/disassembly/ghidra_project/ ghidraBenchmarking_MainProcess -import /home/nahid/reverse/codes/ML/data/binaries/array -overwrite -scriptPath /home/nahid/reverse/disassembly -preScript dwarf_line.py -postScript ghidra_only_instructions.py
 
 
 /home/nahid/reverse_tools/ghidra/ghidra_10.2_PUBLIC_20221101/ghidra_10.2_PUBLIC/support/analyzeHeadless /home/nahid/reverse/disassembly/ghidra_project/ ghidraBenchmarking_MainProcess -import /home/nahid/reverse/codes/ML/data/binaries/array -overwrite -scriptPath /home/nahid/reverse/disassembly -preScript dwarf_line.py -postScript ghidra_slice.py
